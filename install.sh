f54353d5b8ab62bac2f0d517d32585a9b51d212d
welsny <zengw@mssm.org>
2018-01-25 15:57:59 +0800

    add script for saving CMC data as csv

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir "$HOME/cmc_data/"
+mkdir "$HOME/cmc_data/global_data"
+mkdir "$HOME/cmc_data/coin_data"
+
---
+++ save_cmc_data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import pandas as pd
+import requests
+from datetime import datetime
+
+"""
+Saves data from CMC into .csv format. N_COINS and DELAY are configurable below.
+"""
+
+DIR = '~/cmc_data'
+N_COINS = 15
+DELAY = 5*60
+
+if __name__ == '__main__':
+    while True:
+        time_str = datetime.now().strftime("%Y-%m-%d--%H%M")
+        # Individual Coin Data
+        df = pd.read_json(f'https://api.coinmarketcap.com/v1/ticker/?limit={N_COINS}')
+        df.to_csv(f'{DIR}/coin_data/{time_str}.csv', index=False)
+        # Global Data
+        json = requests.get('https://api.coinmarketcap.com/v1/global/').json()
+        df = pd.DataFrame([json])
+        df.to_csv(f'{DIR}/global_data/{time_str}.csv', index=False)
+
+        time.sleep(DELAY)
+
