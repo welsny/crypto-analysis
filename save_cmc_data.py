b632480d8efdf04a30d8e4986895fd40c64a35d7
welsny <zengw@mssm.org>
2018-02-03 22:48:33 +0800

    data selection and reordering in save_cmc_data.py ; make install.sh executable

--- save_cmc_data.py
+++ save_cmc_data.py
@@ -18,13 +18,20 @@ DELAY = 5*60
 if __name__ == '__main__':
     while True:
         time_str = datetime.now().strftime(TIME_FORMAT)
+
         # Individual Coin Data
         df = pd.read_json(f'https://api.coinmarketcap.com/v1/ticker/?limit={N_COINS}')
+        df = df[['rank', 'symbol', 'price_usd', 'market_cap_usd', '24h_volume_usd',
+                 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']]
+        df = df.infer_objects()
         df.to_csv(f'{DIR}/coin_data/{time_str}.csv', index=False)
+
         # Global Data
         json = requests.get('https://api.coinmarketcap.com/v1/global/').json()
-        df = pd.DataFrame([json])
-        df.to_csv(f'{DIR}/global_data/{time_str}.csv', index=False)
+        gdf = pd.DataFrame([json]).infer_objects()
+        gdf = gdf[['total_market_cap_usd', 'total_24h_volume_usd',
+                   'active_markets', 'active_currencies', 'active_assets']]
+        gdf.to_csv(f'{DIR}/global_data/{time_str}.csv', index=False)
 
         time.sleep(DELAY)
 
